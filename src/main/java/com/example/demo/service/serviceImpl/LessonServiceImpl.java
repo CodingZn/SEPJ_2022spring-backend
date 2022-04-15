90a63238c365f4d3b233819d15c553116f20f496
xintianle <20307110196@fudan.edu.cn>
2022-04-15 10:53:27 +0800

    稳定的版本

---
+++ src/main/java/com/example/demo/service/serviceImpl/LessonServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.service.serviceImpl;
+
+import com.example.demo.service.GeneralService;
+
+import java.util.List;
+
+public class LessonServiceImpl implements GeneralService {
+    @Override
+    public String getANewId() {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllIds() {
+        return null;
+    }
+}
---
+++ src/main/java/com/example/demo/service/serviceImpl/MajorServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.service.serviceImpl;
+
+import com.example.demo.service.GeneralService;
+
+import java.util.List;
+
+public class MajorServiceImpl implements GeneralService {
+    @Override
+    public String getANewId() {
+        return null;
+    }
+
+    @Override
+    public List<String> getAllIds() {
+        return null;
+    }
+}
