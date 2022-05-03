12c7e8aa56494adb7661fd7741220f6366da3859
xintianle <20307110196@fudan.edu.cn>
2022-05-03 14:07:21 +0800

    add: 添加了Junit模块

---
+++ src/main/java/com/example/test/MajoridGeneratorTest.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+import com.example.demo.bean.Major;
+import org.junit.Test;
+
+
+public class MajoridGeneratorTest {
+
+    @Test
+    public void testMajoridGenerator(){
+        Major major = new Major();
+        major.setName("测试专业");
+
+    }
+}
