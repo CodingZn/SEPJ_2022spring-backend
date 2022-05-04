85df4f438a4c011bd8a8ec752cbb811bf22dbf13
xintianle <20307110196@fudan.edu.cn>
2022-05-04 16:45:19 +0800

    save: ClassarrangeMapper

--- src/main/java/com/example/demo/mapper/ClassarrangeMapper.java
+++ src/main/java/com/example/demo/mapper/ClassarrangeMapper.java
@@ -1,9 +1,14 @@
 package com.example.demo.mapper;
 
 import com.example.demo.bean.Classarrange;
+import com.example.demo.bean.Classroom;
+import com.example.demo.bean.Classtime;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClassarrangeMapper extends JpaRepository <Classarrange, Integer> {
+    List<Classarrange> findAllByClassroomAndClasstime(Classroom classroom, Classtime classtime);
 }
